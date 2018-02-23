d3abfc811cccc70fdcc14b90f32b3690bcc36e38
seiichinagai <seiichinagai@gmail.com>
2018-02-23 01:57:33 -0800

    getmovieinfo
    simple php doc with variable assignments from html file

---
+++ getmovieinfo.php
@@ -0,0 +1,28 @@
+<?php
+	// Movie Variables
+	$movieName = $_POST['moviename'];
+	$movieID = $_POST['movieid'];
+	$yearReleased = $_POST['yearreleased'];
+	$runTime = $_POST['runtime'];
+	$rating = $_POST['mpaarating'];
+	$bestPicture = $_POST['wonbestpicture'];
+
+	// Genre Variables
+	$genreName = $_POST['genrename'];
+
+	// Writer Variables
+	$writerName = $_POST['writername'];
+	$dobWriter = $_POST['dateofbirthwriter'];
+	$wonOscarWriter = $_POST['wonoscarwriter'];
+
+	// Director Variables
+	$directorName = $_POST['directorname'];
+	$dobDirector = $_POST['dateofbirthdirector'];
+	$wonOscarDirector = $_POST['wonoscardirector'];
+
+	// Lead Actor Variables
+	$actorName = $_POST['actorname'];
+	$dobActor = $_POST['dateofbirthactor'];
+	$wonOscarActor = $_POST['wonoscaractor'];
+	$characterName = $_POST['charactername'];
+?>
